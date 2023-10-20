256c7fe8e1dc9a70890256cb54aedcd459cba70f
Amang <71009153+R-mang@users.noreply.github.com>
2023-10-20 08:54:19 +0000

    feat: init

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-4+ubuntu20.04_all.deb
+dpkg -i zabbix-release_6.0-4+ubuntu20.04_all.deb
+apt update
+apt install zabbix-agent2 zabbix-agent2-plugin-*
+systemctl restart zabbix-agent2
+systemctl enable zabbix-agent2
